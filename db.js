b9eee31126e34925275e24b34f70a3cbee54dd50
xuzhihang <xuzhihang@yy.com>
2023-06-27 16:40:11 +0800

    feat: 修改路由

--- db.js
+++ db.js
@@ -4,7 +4,6 @@ const { Sequelize, DataTypes } = require("sequelize");
 const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = "" } = process.env;
 
 const [host, port] = MYSQL_ADDRESS.split(":");
-
 const sequelize = new Sequelize("nodejs_demo", MYSQL_USERNAME, MYSQL_PASSWORD, {
   host,
   port,
---
+++ router/api.js
@@ -0,0 +1,38 @@
+const router = require('koa-router')()
+const { Counter } = require("../db");
+// 更新计数
+router.post("/count", async (ctx) => {
+  const { request } = ctx;
+  const { action } = request.body;
+  if (action === "inc") {
+    await Counter.create();
+  } else if (action === "clear") {
+    await Counter.destroy({
+      truncate: true,
+    });
+  }
+
+  ctx.body = {
+    code: 0,
+    data: await Counter.count(),
+  };
+});
+
+// 获取计数
+router.get("/count", async (ctx) => {
+  const result = await Counter.count();
+
+  ctx.body = {
+    code: 0,
+    data: result,
+  };
+});
+
+// 小程序调用，获取微信 Open ID
+router.get("/wx_openid", async (ctx) => {
+  if (ctx.request.headers["x-wx-source"]) {
+    ctx.body = ctx.request.headers["x-wx-openid"];
+  }
+});
+
+module.exports = router
